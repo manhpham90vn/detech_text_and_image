d52d074420e00c7ae4e6d3ff57eddf5eca72004d
Manh Pham <manhpham90vn@icloud.com>
2024-12-22 22:06:10 +0700

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
+import re
+import sys
+
 import cv2
 import numpy as np
 import pytesseract
-import os
-import sys
-import re
 
 
 def find_image_locations(large_gray, small_gray, threshold=0.85):
@@ -25,10 +26,7 @@ def extract_text_from_roi(roi):
     roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     roi_denoised = cv2.fastNlMeansDenoising(roi_gray, None, 30, 7, 21)
 
-    text = pytesseract.image_to_string(
-        roi_denoised,
-        config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789KM.'
-    )
+    text = pytesseract.image_to_string(roi_denoised, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789KM.')
 
     text = re.sub(r'[^0-9.KM]', '', text)
 
@@ -60,17 +58,14 @@ def non_max_suppression(boxes, overlapThresh=0.3):
         w = np.maximum(0, xx2 - xx1 + 1)
         h = np.maximum(0, yy2 - yy1 + 1)
         overlap = (w * h) / area[idxs[:-1]]
-        idxs = np.delete(idxs, np.concatenate(([len(idxs) - 1],
-                                               np.where(overlap > overlapThresh)[0])))
+        idxs = np.delete(idxs, np.concatenate(([len(idxs) - 1], np.where(overlap > overlapThresh)[0])))
     return [boxes[i] for i in pick], [boxes[i][5] for i in pick]
 
 
 assets_folder = "assets"
 imgs_folder = "imgs"
-assets_images = [f for f in os.listdir(
-    assets_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
-imgs_images = [f for f in os.listdir(
-    imgs_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+assets_images = [f for f in os.listdir(assets_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+imgs_images = [f for f in os.listdir(imgs_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
 try:
     for img in imgs_images:
@@ -82,16 +77,14 @@ try:
         for asset in assets_images:
             small_image = cv2.imread(os.path.join(assets_folder, asset))
             small_gray = cv2.cvtColor(small_image, cv2.COLOR_BGR2GRAY)
-            locations, result = find_image_locations(
-                large_gray, small_gray)
+            locations, result = find_image_locations(large_gray, small_gray)
 
             for pt in zip(*locations[::-1]):
                 x_start = pt[0] - 15
                 y_start = pt[1] + small_image.shape[0] + 2
                 x_end = pt[0] + small_image.shape[1] + 15
                 y_end = pt[1] + small_image.shape[0] + 35
-                boxes.append([x_start, y_start, x_end, y_end,
-                             result[pt[1], pt[0]], asset])
+                boxes.append([x_start, y_start, x_end, y_end, result[pt[1], pt[0]], asset])
 
         boxes, asset_names = non_max_suppression(boxes, overlapThresh=0.5)
 
@@ -103,10 +96,8 @@ try:
             roi = large_image[y_start:y_end, x_start:x_end]
 
             text = extract_text_from_roi(roi)
-            print(f"Text in the expanded region below object from asset '{
-                  asset_name}' in image '{img}': {text}")
-            cv2.rectangle(large_image, (x_start, y_start),
-                          (x_end, y_end), (255, 0, 0), 2)
+            print(f"Text in the expanded region below object from asset '{asset_name}' in image '{img}': {text}")
+            cv2.rectangle(large_image, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
 
         cv2.imshow(f'Result for {img}', large_image)
         cv2.waitKey(0)
